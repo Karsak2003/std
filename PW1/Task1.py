aac6d09e13ac969668192a3d65f0fd3b980a64ab
Rustam Karimov <Karim-Sacaev@yandex.ru>
2024-10-02 15:25:06 +0500

    Добавлен первая задача из первой практической

---
+++ PW1/Task1.py
@@ -0,0 +1,11 @@
+#
+#region IMPORTS
+import random as rhd
+#endregion IMPORTS
+
+
+def _main_():
+    print("Введите список чисел или слово 'RANDOM' и длину списка")
+    pass
+
+if __name__ == "__main__": _main_()
