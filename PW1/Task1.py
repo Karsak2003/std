7f04b7b93b6b002a520ee03305c6f01f46ca66d9
Rustam Karimov <Karim-Sacaev@yandex.ru>
2024-10-02 17:17:48 +0500

    Выполнино первое задание первой практической

--- PW1/Task1.py
+++ PW1/Task1.py
@@ -1,11 +1,19 @@
 #
 #region IMPORTS
-import random as rhd
+import random as rnd
 #endregion IMPORTS
 
 
-def _main_():
-    print("Введите список чисел или слово 'RANDOM' и длину списка")
-    pass
+def _main_() -> None:
+    print("Введите список чисел или слово 'RANDOM'")
+    n = list(input("Список:").lower().split())
+    if isinstance(n[0], int) and (not n[0] in "randomrnd"):
+        print(f"Исходный список: {" ".join(n)}")
+        print(f"обратный список: {" ".join(n[::-1])}")
+    else:
+        l, r, c = map(int, input("Введите диапазон (левый и правый края) и длину списка - необходимо з числа:\n").split())
+        a = [str(rnd.randint(min(l, r), max(l, r))) for _ in range(c)]
+        print(f"Исходный список: {" ".join(a)}")
+        print(f"обратный список: {" ".join(a[::-1])}")
 
 if __name__ == "__main__": _main_()
