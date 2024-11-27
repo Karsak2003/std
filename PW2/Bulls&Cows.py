0f54e785b4726b914ab5e82a08feb0da254cc187
Rustam Karimov <Karim-Sacaev@yandex.ru>
2024-11-27 16:33:40 +0500

    -

--- PW2/Bulls&Cows.py
+++ PW2/Bulls&Cows.py
@@ -4,11 +4,16 @@ import random
 import res
 #endregion IMPORTS
 
+
+
+
 def main() -> None:
-    numbers = random.sample(range(0, 10), 4)
-    
+    numbers = [random.randint(1, 10)] + random.sample(range(0, 10), 3)
     print("\n".join(res.openName))
-    
+    input("\n\n\nPlease press the 'ENTER' button to start the game...")
+    while True:
+        
+        pass
     print(numbers)
     print("\n".join(res.bull))
     print("\n".join(res.cow))
