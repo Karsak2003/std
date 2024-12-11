9f2a9dbf837ea6e40aecba74617ab255e8c569a4
Rustam Karimov <Karim-Sacaev@yandex.ru>
2024-12-11 18:14:39 +0500

    Исправление ошибок

--- PW1/Task3.py
+++ PW1/Task3.py
@@ -13,7 +13,7 @@ def _main_() -> None:
         l, r, c = map(int, input("Введите диапазон (левый и правый края) и длину списка - необходимо з числа:\n").split())
         n = [str(rnd.randint(min(l, r), max(l, r))) for _ in range(c)]
         print(f"Исходный список: {" ".join(n)}")
-    ns = set(n)
-    print(ns)
+    ns = list(set(n))
+    print(" ".join(ns))
 
 if __name__ == "__main__": _main_()
---
+++ PW2/RPS.py
@@ -0,0 +1,29 @@
+
+#region IMPORTS
+import random
+#endregion IMPORTS
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main() -> None:
+    print("✂️🪨🗞️")
+    
+    pass
+
+
+
+
+
+
+
+if __name__ == "__main__":main()
